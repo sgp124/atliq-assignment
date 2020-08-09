a4e2745a0d9b2f7a127202cbb353d120ea48a4f3
Shail Patel <shailpatel124@gmail.com>
2020-08-09 14:18:07 +0530

    feature: simple file reading to pandas dataframe

--- ingestion.py
+++ ingestion.py
@@ -1,8 +1,13 @@
 import pandas as pd
 
+data_directory = "data/"
+file_extention = ".xlsx"
+
 def start_ingestion():
 	print("ingestion started")
+	df = pd.read_excel(data_directory+"Vendor Transactions"+file_extention)
+	print(df)
 	
 
 if __name__ == "__main__":
-	start_ingestion();
+	start_ingestion()
