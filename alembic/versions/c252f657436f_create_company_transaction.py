db213d5eb5d07ef770cdb448ffb4fd9436729b05
Shail Patel <shailpatel124@gmail.com>
2020-08-09 15:46:56 +0530

    added:company_transaction_table

---
+++ alembic/versions/c252f657436f_create_company_transaction.py
@@ -0,0 +1,36 @@
+"""create company_transaction
+
+Revision ID: c252f657436f
+Revises: 42fc3e51232a
+Create Date: 2020-08-09 15:37:37.428428
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'c252f657436f'
+down_revision = '42fc3e51232a'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table(
+        'company_transcation',
+        sa.Column('store',sa.String(20)),
+        sa.Column('date',sa.DateTime),
+        sa.Column('trans_id',sa.Numeric,primary_key=True),
+        sa.Column('coustmer_id',sa.Numeric),
+        sa.Column('imei', sa.Numeric),
+        sa.Column('phone', sa.Numeric),
+        sa.Column('trans_type',sa.String(20)),
+        sa.Column('state',sa.String(20))
+    )
+    pass
+
+
+def downgrade():
+    op.drop_table('company_transcation')
+    pass
