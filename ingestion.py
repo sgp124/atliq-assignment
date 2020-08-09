3af2bb32b0efbd91a2b74c04790cae1961e29c91
Shail Patel <shailpatel124@gmail.com>
2020-08-09 16:17:45 +0530

    added :  ingest vendor transactions  to database

--- ingestion.py
+++ ingestion.py
@@ -1,13 +1,15 @@
 import pandas as pd
+import db
 
 data_directory = "data/"
 file_extention = ".xlsx"
 
-def start_ingestion():
-	print("ingestion started")
+def ingest_vendor_transaction():
 	df = pd.read_excel(data_directory+"Vendor Transactions"+file_extention)
+	df.columns = ['date','cost','imei','phone','sales_type']
 	print(df)
-	
+
+	df.to_sql('vendor_transactions',db.engine,if_exists = 'append',index_label='id')
 
 if __name__ == "__main__":
-	start_ingestion()
+	ingest_vendor_transaction()
