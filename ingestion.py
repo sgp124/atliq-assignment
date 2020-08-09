f6dfae52937ca1ce9dd62c8d5c7115e9dc54ca40
Shail Patel <shailpatel124@gmail.com>
2020-08-09 16:32:41 +0530

    added:ingetion of vendor sales type,store master,company trans

--- ingestion.py
+++ ingestion.py
@@ -8,8 +8,31 @@ def ingest_vendor_transaction():
 	df = pd.read_excel(data_directory+"Vendor Transactions"+file_extention)
 	df.columns = ['date','cost','imei','phone','sales_type']
 	print(df)
+	df.to_sql('vendor_transactions',db.engine,if_exists = 'replace',index_label='id')
 
-	df.to_sql('vendor_transactions',db.engine,if_exists = 'append',index_label='id')
+
+def ingest_vendor_sales_types():
+	df = pd.read_excel(data_directory+"Vendor Sales Type"+file_extention)
+	df.columns = ['code','sales_type']
+	print(df)
+	df.to_sql('vendor_sales_types',db.engine,if_exists = 'replace',index_label='id')
+
+
+def ingest_store_master():
+	df = pd.read_excel(data_directory+"Store Master"+file_extention)
+	df.columns = ['code','store','city','state']
+	print(df)
+	df.to_sql('store_master',db.engine,if_exists = 'replace',index_label='id')
+
+
+def ingest_company_transcation():
+	df = pd.read_excel(data_directory+"Company Trans"+file_extention)
+	df.columns = ['store','date','trans_id','coustmer_id','imei','phone','trans_type','state']
+	print(df)
+	df.to_sql('company_transcation',db.engine,if_exists = 'replace',index_label='id')
 
 if __name__ == "__main__":
 	ingest_vendor_transaction()
+	ingest_vendor_sales_types()
+	ingest_store_master()
+	ingest_company_transcation()
